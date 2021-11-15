ccf9d1ca659f52f3732322c8168d8a4e1fa6ab53
D Kornbluth <dwight.kornbluth@gmail.com>
2021-11-15 13:20:03 -0500

    updated db.js

--- public/db.js
+++ public/db.js
@@ -19,3 +19,39 @@ request.onsuccess = ({ e }) => {
 request.onerror = (e) => {
     console.log(`Doh! ${e.target.errorCode}`);
 };
+
+function dbCheck() {
+    let transaction = db.transaction(["waitForIt"], "readwrite");
+    let track = transaction.objectStore("waitForIt");
+
+    let data = track.data();
+
+    data.onsuccess = function() {
+        // check that data is not empty and grab it and post it, then remove from cache
+        if(data.result.length > 0) {
+            fetch("/api/transaction/bulk", {
+                method: "POST",
+                body: JSON.stringify(data.result),
+                headers: {
+                    "Content-Type":"application/json"
+                }
+            })
+            .then(res => {
+                return res.json();
+            })
+            .then(() => {
+                let transaction = db.transaction(["waitForIt"], "readwrite");
+                let track = transaction.objectStore("waitForIt");
+
+                track.clear();
+            });
+        }
+    };
+}
+
+function saveIt(data) {
+    let transaction = db.transaction(["waitForIt"], "readwrite");
+    const track = transaction.objectStore("waitForIt");
+
+    track.add(data);
+}
